45a3a27251f82b21e1b978d9416c17ac8124e4f4
jedsantillan <jedsantillan811@gmail.com>
2024-12-20 15:53:58 +0800

    Add project files.

---
+++ Todo.Domain/Entities/TodoTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Domain.Entities
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool isDone { get; set; } = false;
+    }
+}
---
+++ Todo.Domain/Interfaces/ITodoTaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Interfaces
+{
+    public interface ITodoTaskRepository
+    {
+        Task<List<TodoTask>> GetAllTodoTasksAsync();
+        Task<TodoTask> GetTodoTaskByIdAsync();
+        Task AddTodoTaskAsync(TodoTask todoTask);
+        Task DeleteTodoTaskAsync(int id);
+
+    }
+}
